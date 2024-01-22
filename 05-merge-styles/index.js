26b0fd9fd1a28447aac227732802b80689e7efea
AL424 <algr5113@gmail.com>
2024-01-22 21:36:04 +0300

    feat: add bundleStyles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,11 +5,16 @@ const { promises } = fs;
 const dirStyles = path.join(__dirname, 'styles');
 const bundle = path.join(__dirname, 'project-dist', 'bundle.css');
 
-promises
-  .readdir(dirStyles)
-  .then((files) => files.filter((file) => path.extname(file) === '.css'))
-  .then((files) => files.map((file) => path.join(dirStyles, file)))
-  .then((files) => files.map((file) => promises.readFile(file, 'utf-8')))
-  .then((files) => Promise.all(files))
-  .then((files) => files.join('\n'))
-  .then((styles) => promises.writeFile(bundle, styles));
+const bundleStyles = (dir, bundle) =>
+  promises
+    .readdir(dir)
+    .then((files) => files.filter((file) => path.extname(file) === '.css'))
+    .then((files) => files.map((file) => path.join(dir, file)))
+    .then((files) => files.map((file) => promises.readFile(file, 'utf-8')))
+    .then((files) => Promise.all(files))
+    .then((files) => files.join('\n'))
+    .then((styles) => promises.writeFile(bundle, styles));
+
+bundleStyles(dirStyles, bundle);
+
+module.exports = bundleStyles;
