7e04a4269d7682d87d46a65755cde63456421278
AL424 <algr5113@gmail.com>
2024-01-22 21:35:00 +0300

    feat: update copyDir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,27 +1,26 @@
 const fs = require('fs');
 const path = require('path');
-
 const { promises } = fs;
 
 const copyDir = (dir, copy) => {
-  promises
-    .mkdir(copy, { recursive: true })
-    .then(() => promises.readdir(dir))
-    .then((files) => {
-      files.forEach((fileName) => {
-        const file = path.join(dir, fileName);
-        const fileCopy = path.join(copy, fileName);
+  promises.rm(copy, { recursive: true, force: true }).then(() => {
+    promises
+      .mkdir(copy, { recursive: true })
+      .then(() => promises.readdir(dir))
+      .then((files) => {
+        files.forEach((fileName) => {
+          const file = path.join(dir, fileName);
+          const fileCopy = path.join(copy, fileName);
 
-        promises.stat(file).then((stat) => {
-          if (stat.isDirectory()) copyDir(file, fileCopy);
-          else promises.copyFile(file, fileCopy);
+          promises.stat(file).then((stat) => {
+            if (stat.isDirectory()) copyDir(file, fileCopy);
+            else promises.copyFile(file, fileCopy);
+          });
         });
       });
-    });
+  });
 };
 
-promises
-  .rm(path.join(__dirname, 'files-copy'), { recursive: true, force: true })
-  .then(() =>
-    copyDir(path.join(__dirname, 'files'), path.join(__dirname, 'files-copy')),
-  );
+copyDir(path.join(__dirname, 'files'), path.join(__dirname, 'files-copy'));
+
+module.exports = copyDir;
